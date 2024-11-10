7fe462b04c8bf3a1e9c46b4e9b121c5074f33ae1
AMarchmarti <marchmarti.antoni@gmail.com>
2024-11-10 16:38:37 +0100

    feature[Arquitecture]: added the arquitecture we follow, hexagonal arquitecture

---
+++ test/vite.setup.tsx
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import React from "react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+
+export function renderWithRouter(children: any, routes = []) {
+	const options = { element: children, path: "/" };
+
+	const router = createMemoryRouter([{ ...options }, ...routes], {
+		initialEntries: [options.path],
+		initialIndex: 1,
+	});
+
+	render(<RouterProvider router={router} />);
+	return router;
+}
+
+export {
+	fireEvent,
+	screen,
+	waitFor,
+	getByRole,
+	within,
+	act,
+} from "@testing-library/react";
